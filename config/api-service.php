90ac66d9b5d50563eef47667ff8af10d614a33c1
Daffa Yuslim <andidaffa75@gmail.com>
2024-02-19 15:10:06 +0800

    Revert "Revert "feat: update api plugin""
    This reverts commit d3f33a7ae0444f91e5400e5d7547fda3cc09faf7.

---
+++ config/api-service.php
@@ -0,0 +1,15 @@
+<?php
+
+// config for Rupadana/ApiService
+return [
+    'navigation' => [
+        'group' => [
+            'token' => 'Website',
+        ],
+    ],
+    'models' => [
+        'token' => [
+            'enable_policy' => true,
+        ],
+    ],
+];
