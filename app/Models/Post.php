57ec1b6280ead9046f249f390104d404c324d699
Daffa Yuslim <andidaffa75@gmail.com>
2024-02-21 11:12:54 +0800

    Revert "fix: bugs"
    This reverts commit eaf6040f0fec27ce7711e77f4592937fb220440a.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -50,6 +50,10 @@ class Post extends Model implements HasMedia
         'status',
     ];
 
+    protected $casts = [
+        'published_at' => 'datetime'
+    ];
+
     public function created_by(): BelongsTo
     {
         return $this->belongsTo(User::class, 'created_by');
