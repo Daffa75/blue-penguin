203a5e84ab73366f0d23a44ab859c9fc58f35390
Daffa Yuslim <andidaffa75@gmail.com>
2024-04-19 08:16:47 +0800

    feat: publication api

---
+++ app/Filament/Publication/Resources/PublicationResource/Api/Handlers/DetailHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filament\Publication\Resources\PublicationResource\Api\Handlers;
+
+use App\Filament\Resources\SettingResource;
+use App\Filament\Publication\Resources\PublicationResource;
+use Rupadana\ApiService\Http\Handlers;
+use Spatie\QueryBuilder\QueryBuilder;
+use Illuminate\Http\Request;
+
+class DetailHandler extends Handlers
+{
+    public static string | null $uri = '/{id}';
+    public static string | null $resource = PublicationResource::class;
+
+
+    public function handler(Request $request)
+    {
+        $id = $request->route('id');
+        
+        $model = static::getEloquentQuery();
+
+        $query = QueryBuilder::for(
+            $model->where(static::getKeyName(), $id)
+        )
+            ->with('lecturers')
+            ->first();
+
+        if (!$query) return static::sendNotFoundResponse();
+
+        $transformer = static::getApiTransformer();
+
+        return new $transformer($query);
+    }
+}
