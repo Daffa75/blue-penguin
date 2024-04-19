d224a5839b2d3bbfa60ba7c0b06753cf89d84104
Syawal Achmad <syawalachmad1323@gmail.com>
2024-04-19 20:07:38 +0800

    feat: add export feature action to final project

---
+++ app/Exports/FinalProjectsExport.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Exports;
+
+use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Sheet;
+
+class FinalProjectsExport implements FromView, WithEvents, Responsable
+{
+    use Exportable;
+
+    private $data;
+    private $fileName;
+    private $url;
+
+    public function __construct($data, $url)
+    {
+        $this->fileName = now()->format('Y-m-d_his') . '-inventaris.xlsx';
+
+        $this->data = $data;
+
+        $this->url = $url;
+    }
+
+    public function view(): View
+    {
+        $data['data_final_projects'] = $this->data;
+
+        $data['url'] = $this->url;
+
+        return view('exports.final-projects', $data);
+    }
+
+    public function registerEvents(): array
+    {
+        Sheet::macro('styleCells', function (Sheet $sheet, string $cellRange, array $style) {
+            $sheet->getDelegate()->getStyle($cellRange)->applyFromArray($style);
+        });
+
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                $sheet = $event->sheet->getDelegate();
+                $highestRow = $sheet->getHighestRow();
+                $highestColumn = $sheet->getHighestColumn();
+                $range = 'A1:' . $highestColumn . $highestRow;
+
+                foreach (range('A', $highestColumn) as $column) {
+                    if ($column != 'C') {
+                        $sheet->getColumnDimension($column)
+                            ->setAutoSize(true);
+                    } else {
+                        $sheet->getColumnDimension($column)
+                            ->setWidth(25);
+                    }
+                }
+
+                $event->sheet->styleCells(
+                    $range,
+                    [
+                        'font' => [
+                            'name' => 'Arial',
+                            'size' => 10,
+                        ],
+                    ]
+                );
+
+                // for header styling
+                $event->sheet->styleCells(
+                    'A1:' . $highestColumn . 1,
+                    [
+                        'alignment' => [
+                            'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                        ],
+                        'font' => [
+                            'bold' => true,
+                        ],
+                        'fill' => [
+                            'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                            'color' => ['argb' => '9CC2E5']
+                        ]
+                    ]
+                );
+
+                $borderRange = $highestColumn . $highestRow;
+                $event->sheet->styleCells(
+                    'A1:' . $borderRange,
+                    [
+                        'borders' => [
+                            'allBorders' => [
+                                'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                            ],
+                        ],
+                    ]
+                );
+            }
+        ];
+    }
+}
