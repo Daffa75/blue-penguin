363fe24bf74aadb229d4de950edcdae405ea4b0e
FrhnSpwli <andifarhansappewali407@gmail.com>
2024-04-19 14:32:58 +0800

    add student panel

---
+++ app/Providers/Filament/StudentPanelProvider.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Providers\Filament;
+
+use App\Filament\Auth\CustomLogin;
+use App\Filament\Student\Pages\Auth\CustomProfile;
+use App\Filament\Student\Pages\Dashboard;
+use App\Filament\Widgets\AccountOverviewCustom;
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Pages;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Widgets;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Rupadana\ApiService\ApiServicePlugin;
+
+class StudentPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->id('student')
+            ->path('student')
+            ->colors([
+                'primary' => Color::Amber,
+            ])
+            ->login(CustomLogin::class)
+            ->profile(CustomProfile::class)
+            ->discoverResources(in: app_path('Filament/Student/Resources'), for: 'App\\Filament\\Student\\Resources')
+            ->resources([
+                \App\Filament\Admin\Resources\InternshipResource::class,
+            ])
+            ->discoverPages(in: app_path('Filament/Student/Pages'), for: 'App\\Filament\\Student\\Pages')
+            ->pages([
+                Dashboard::class,
+            ])
+            ->discoverWidgets(in: app_path('Filament/Student/Widgets'), for: 'App\\Filament\\Student\\Widgets')
+            ->widgets([
+                AccountOverviewCustom::class,
+            ])
+            ->sidebarCollapsibleOnDesktop()
+            ->plugins([
+                ApiServicePlugin::make()
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+            ->authMiddleware([
+                Authenticate::class,
+            ]);
+    }
+}
