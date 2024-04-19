8778dbb5c5b46cc0bb11b7f9fbe551856ac02a82
FrhnSpwli <andifarhansappewali407@gmail.com>
2024-04-19 14:32:50 +0800

    add lecturer panel

---
+++ app/Providers/Filament/LecturerPanelProvider.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Providers\Filament;
+
+use App\Filament\Admin\Pages\Auth\CustomProfile;
+use App\Filament\Auth\CustomLogin;
+use App\Filament\Widgets\AccountOverviewCustom;
+use App\Filament\Widgets\PublicationChart;
+use App\Filament\Widgets\PublicationLineChart;
+use App\Filament\Publication\Pages\Dashboard;
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Pages;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Widgets;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+
+use Rupadana\ApiService\ApiServicePlugin;
+use Althinect\FilamentSpatieRolesPermissions\FilamentSpatieRolesPermissionsPlugin;
+
+class LecturerPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->id('lecturer')
+            ->path('lecturer')
+            ->colors([
+                'primary' => Color::Amber,
+            ])
+            ->login(CustomLogin::class)
+            ->profile(CustomProfile::class)
+            ->discoverResources(in: app_path('Filament/Lecturer/Resources'), for: 'App\\Filament\\Lecturer\\Resources')
+            ->discoverResources(in: app_path('Filament/Publication/Resources'), for: 'App\\Filament\\Publication\\Resources')
+            ->discoverResources(in: app_path('Filament/FinalProject/Resources'), for: 'App\\Filament\\FinalProject\\Resources')
+            ->resources([
+                \App\Filament\Admin\Resources\InternshipResource::class,
+            ])
+            ->discoverPages(in: app_path('Filament/Lecturer/Pages'), for: 'App\\Filament\\Lecturer\\Pages')
+            ->pages([
+                Pages\Dashboard::class,
+            ])
+            ->widgets([
+                AccountOverviewCustom::class,
+                PublicationChart::class,
+                PublicationLineChart::class,
+            ])
+            ->sidebarCollapsibleOnDesktop()
+            ->plugins([
+                ApiServicePlugin::make(),
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+            ->authMiddleware([
+                Authenticate::class,
+            ]);
+    }
+}
